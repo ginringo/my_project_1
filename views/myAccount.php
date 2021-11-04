9bb9a7544c08535805038c9e2d1f39051526ed2b
ginringo <akairingo.gn@gmail.com>
2021-11-04 22:06:20 +0900

    アカウントページ作成

---
+++ views/myAccount.php
@@ -0,0 +1,19 @@
+<form action="" method="post" class="row">
+    <div class="col-sm-6 offset-sm-3 mt-5 mb-5">
+        <!-- 会員情報の変更に関するメッセージ -->
+        <div class="d-flex justify-content-between align-items-center">
+            <p>hogeさん、こんにちは</p>
+            <div class="link">
+                <table class="table table-bordered text-center">
+                    <tr>
+                        <td><a href="changeInfo.php">登録情報を変更する</a></td>
+                    </tr>
+                    <tr>
+                        <td><a href="login.php">ログアウト</a></td>
+                    </tr>
+                </table>
+            </div>
+        </div>
+        <hr class="mt-0">
+    </div>
+</form>
