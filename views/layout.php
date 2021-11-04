598035e79232e1cfeb9b9da129283c55e074e8cd
ginringo <akairingo.gn@gmail.com>
2021-11-04 22:27:43 +0900

    CSSファイルのファイル名変更

--- views/layout.php
+++ views/layout.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $title ?>&nbsp;/&nbsp;Hoge</title>
 
-    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/style.css">
     <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans+SC:wght@100&family=Share+Tech&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 </head>
