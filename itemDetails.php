94bb0667931a2b2e39d1af2181cac8c5313e80c6
ginringo <akairingo.gn@gmail.com>
2021-11-06 22:59:08 +0900

    商品詳細ページ作成

---
+++ itemDetails.php
@@ -0,0 +1,8 @@
+<?php
+
+$title = 'Item Details';
+$content = __DIR__ . '/views/itemDetails.php';
+$headLineEn = 'Item Details / ';
+$headLineJa = 'アイテム詳細';
+
+include __DIR__ . '/views/layout.php';
---
+++ views/itemDetails.php
@@ -0,0 +1,34 @@
+<div class="row">
+    <div class="col-sm-10 offset-sm-1 d-flex flex-wrap">
+        <div class="itemImgWrap">
+            <img class="itemImg" src="../img/item1.png" alt="">
+        </div>
+        <div class="sentence mt-3 d-flex align-content-between flex-wrap">
+            <div>
+                <h3 class=" mb-3">hogehogehogehoge</h3>
+                <p>hogehogehoge</p>
+                <p>hogehogehoge</p>
+                <p>hogehogehoge</p>
+                <p>hogehogehoge</p>
+                <p>hogehogehoge</p>
+                <p>hogehogehoge</p>
+                <p>hogehogehoge</p>
+                <p>hogehogehoge</p>
+            </div>
+
+            <form action="" method="post" class="d-flex">
+                <div class="form-group mr-2">
+                    <select id="category" name="category" class="form-control">
+                        <option value="">数量</option>
+                        <option value="">1</option>
+                        <option value="">2</option>
+                        <option value="">3</option>
+                    </select>
+                </div>
+                <div>
+                    <button type="submit" class="btn btn-dark">カートに追加</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
