eaa5dd6ab8da4896876b2e19da6d9691a6219019
ginringo <akairingo.gn@gmail.com>
2021-11-19 00:29:17 +0900

    ログイン、ログアウトのロジックを記述

--- views/login.php
+++ views/login.php
@@ -1,18 +1,22 @@
-<form action="" method="post" class="row">
+<form action="login.php?action=login" method="post" class="row">
     <div class="col-sm-8 offset-sm-2">
 
-        <div class="link mb-1"><a href="registration.php">※&nbsp;会員登録がまだの方はこちらから</a></div>
-        <div class="link mb-3"><a href="#">※&nbsp;パスワードを忘れた方はこちら</a></div>
+        <div class="link"><a href="memberInfoInput.php">会員登録がまだの方はこちらから</a></div>
+        <hr>
+        <?php if (isset($loginError)) : ?>
+            <p class="text-danger">※<?php echo $loginError ?></p>
+        <?php endif; ?>
+        <!-- <div class="link mb-3"><a href="#">※&nbsp;パスワードを忘れた方はこちら</a></div> -->
 
         <!-- メアド -->
         <div class="form-group">
             <label for="email">メールアドレス<?= $req ?></label>
-            <input type="email" class="form-control" id="email" name="email" required>
+            <input type="email" class="form-control" id="email" name="email" value="<?php echo $_POST['email'] ?>" required>
         </div>
         <!-- パスワード -->
         <div class="form-group">
             <label for="pass">パスワード<?= $req ?></label>
-            <input type="password" class="form-control" id="pass" required>
+            <input type="password" class="form-control" id="pass" name="pass" required>
         </div>
         <!-- 送信ボタン -->
         <div class="mt-4">
