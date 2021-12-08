560b0d6b5aed520fcae044cbd333e83d63f524f6
ginringo <akairingo.gn@gmail.com>
2021-12-08 18:34:00 +0900

    商品詳細ページをデータベースに対応

--- itemDetails.php
+++ itemDetails.php
@@ -1,8 +1,13 @@
 <?php
 
+require_once __DIR__ . '/lib/dbComponents.php';
+
 $title = 'Item Details';
 $content = __DIR__ . '/views/itemDetails.php';
 $headLineEn = 'Item Details / ';
 $headLineJa = 'アイテム詳細';
+
+$productSelect = 'SELECT * FROM products WHERE id = ?';
+$product = selectOneRow($productSelect, [$_REQUEST['product_id']]);
 
 include __DIR__ . '/views/layout.php';
