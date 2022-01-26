376f99982a38cd1af32bc912fc95a3bddef2b6bc
ginringo <akairingo.gn@gmail.com>
2022-01-26 23:31:01 +0900

    エラーメッセージとボタンの表示を変更

--- views/login.php
+++ views/login.php
@@ -3,26 +3,25 @@
 
         <div class="link ml-1"><a href="memberInfoInput.php" class="link-line">会員登録がまだの方はこちらから</a></div>
         <hr>
+
         <?php if (isset($loginError)) : ?>
-            <p class="shadow-sm p-2 bg-white rounded text-danger">
+            <p class="pb-1 border-bottom border-danger text-danger">
                 <span class="font-weight-bold">×&nbsp;</span><?php echo $loginError ?>
             </p>
         <?php endif; ?>
-        <!-- <div class="link mb-3"><a href="#">※&nbsp;パスワードを忘れた方はこちら</a></div> -->
 
-        <!-- メアド -->
         <div class="form-group">
             <label for="email">メールアドレス<?= $req ?></label>
             <input type="email" class="form-control" id="email" name="email" value="<?php echo $_POST['email'] ?>" required>
         </div>
-        <!-- パスワード -->
+
         <div class="form-group">
             <label for="pass">パスワード<?= $req ?></label>
             <input type="password" class="form-control" id="pass" name="pass" required>
         </div>
-        <!-- 送信ボタン -->
+
         <div class="mt-4">
-            <button type="submit" class="btn btn-dark">ログイン</button>
+            <button type="submit" class="btn btn-dark btn-block">ログイン</button>
         </div>
     </div>
 </form>
