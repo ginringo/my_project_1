530dffb7af481406c95b7d70f5ffed30b4ba49ab
ginringo <akairingo.gn@gmail.com>
2022-02-01 09:52:43 +0900

    会員登録確認画面のレイアウトを変更

--- views/memberInfoDisplay.php
+++ views/memberInfoDisplay.php
@@ -1,30 +1,43 @@
 <div class="row">
     <div class="col-sm-8 offset-sm-2">
-        <p class="mb-4">※&nbsp;確認後「登録する」ボタンを押してください</p>
-        <hr>
-        <p>お名前</p>
-        <p><?php echo $_SESSION['memberInfo']['name'] ?></p>
-        <hr>
-        <p>住所</p>
-        <p><?php echo $_SESSION['memberInfo']['address'] ?></p>
-        <hr>
-        <p>メールアドレス</p>
-        <p><?php echo $_SESSION['memberInfo']['email'] ?></p>
-        <hr>
-        <p>電話番号</p>
-        <p><?php echo $_SESSION['memberInfo']['tel'] ?></p>
-        <hr>
-        <p>パスワード</p>
-        <p><?php echo preg_replace('/./', '●', $_SESSION['memberInfo']['pass1']) ?></p>
-        <hr>
-        <div class="d-flex mt-4">
-            <div class="mr-4">
-                <button　type="submit" class="btn btn-dark" onclick="location.href='memberInfoInput.php?action=rewrite'">修正する</button>
+
+        <table id="table" class="table text-center mb-4">
+            <tr>
+                <td class="align-middle">お名前</td>
+                <td></td>
+                <td class="align-middle"><?= $_SESSION['memberInfo']['name'] ?></td>
+            </tr>
+            <tr>
+                <td class="align-middle">住所</td>
+                <td></td>
+                <td class="align-middle"><?= $_SESSION['memberInfo']['address'] ?></td>
+            </tr>
+            <tr>
+                <td class="align-middle">メールアドレス</td>
+                <td></td>
+                <td class="align-middle"><?= $_SESSION['memberInfo']['email'] ?></td>
+            </tr>
+            <tr>
+                <td class="align-middle">電話番号</td>
+                <td></td>
+                <td class="align-middle"><?= $_SESSION['memberInfo']['tel'] ?></td>
+            </tr>
+            <tr class="border-bottom">
+                <td class="align-middle">パスワード</td>
+                <td></td>
+                <td class="align-middle"><?= preg_replace('/./', '●', $_SESSION['memberInfo']['pass1']) ?></td>
+            </tr>
+        </table>
+
+        <div class="d-flex mt-5 justify-content-center">
+            <div class="w-100 mr-5 ml-4">
+                <button type="submit" class="btn btn-lg btn-dark btn-block" onclick="location.href='memberInfoInput.php?action=rewrite'">修正する</button>
             </div>
-            <form action="" method="post">
+            <form class="w-100 mr-4" action="#" method="post">
                 <input type="hidden" name="action" value="submit">
-                <button type="submit" class="btn btn-success">登録する</button>
+                <button type="submit" class="btn btn-lg btn-success btn-block">登録する</button>
             </form>
         </div>
+
     </div>
 </div>
